5c3ed2efe5099e3f5fbdf22918dffbd7919a77d9
charles decellieres <charles3397@hotmail.fr>
2019-06-27 15:51:21 +0000

    tajeta completa

--- tarjetas.py
+++ tarjetas.py
@@ -18,22 +18,45 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////home/bastien/Desktop/program
 db = SQLAlchemy(app)
 
 def compare_date(year1,month1,year2,month2):
-	
+	if year1 < year2 :
+		return True
+	elif year1 == year2 :
+		if month1 <= month2 :
+			return True
+		else :
+			return False
+	else :
+		return False
 
 @app.route("/tarjeta", methods = ["GET","POST"] )
 def tarjeta():
 	value = request.json["tarjeta"]
 	card = Card.query.filter_by(id = value).first()
 	if not card :
-		return jsonify({'error':"pas de carte"})
-	return jsonify({'vencimiento':card.vencimiento})
-	card_year = card.vencimiento[:4]
-	card_month = card.vencimiento[4:]
-	date = datetime.date.today()	
-	if card :
-		return jsonify({'vencimiento':card.vencimiento})
-	return value
+		return jsonify({"codError":"20","error":"No existe la tarjeta"}), 403
+	card_date = str(card.vencimiento)
+	card_year = int(card_date[:4])
+	card_month = int(card_date[4:])
+	date = datetime.date.today()
+	if compare_date(2022,date.month,card_year,card_month) == True :
+	#if compare_date(date.year,date.month,card_year,card_month) == True :
+		return render_template('validation_fecha.html'), 201
+	else :
+		return jsonify({"codError":"21","error":"Tarjeta expirada"}), 403	
+		
 
+@app.route("/monto", methods=["GET","POST"])
+def monto():
+	value = request.json["tarjeta"]
+	monto = float(request.json["monto"])
+	card = Card.query.filter_by(id = value).first()
+	if not card :
+		return jsonify({"codError":"20","error":"No existe la tarjeta"}), 403
+	if card.montoMaximo < monto :
+		return jsonify({"codError":"30","error":"Monto maximo de venta superado"}), 403
+	else :
+		return render_template('validation_monto.html'), 201
+	
 
 if __name__ == '__main__':
 	app.run(debug=True)
